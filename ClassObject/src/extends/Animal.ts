67f66e9d14666a022b9b46146dcfd8a272d4c63c
DESKTOP-MHSJV2V\ya596 <ya5969@163.com>
2021-11-28 23:05:56 +0800

    抽象类  抽象方法

--- ClassObject/src/extends/Animal.ts
+++ ClassObject/src/extends/Animal.ts
@@ -1,4 +1,11 @@
-class Animal {
+/**
+ * 以abstract定义的类是抽象类
+ *  - 抽象类和其他类区别不大，只是不能被用来创建实例，即不能通过new关键字实例化对象
+ *  - 抽象类，就是专门用来被继承的类
+ * 抽象类中可以添加抽象方法
+ *  - 
+ */
+abstract class Animal {
     name: string;
     age: number;
 
@@ -19,6 +26,11 @@ class Animal {
     sayHello() {
         console.log("我是动物，Hello！");
     }
+
+    /**
+     * 定义抽象方法：只定义方法体，不做方法实现，可以有方法的返回值类型
+     */
+    abstract introBySelf():void;
 }
 
 export { Animal };
