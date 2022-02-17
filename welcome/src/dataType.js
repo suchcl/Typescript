11c02a6af94bcf85477ac151e116cf1322577c32
DESKTOP-MHSJV2V\ya596 <ya5969@163.com>
2022-02-17 22:22:19 +0800

    元祖越界、对象类型变量的声明

--- welcome/src/dataType.js
+++ welcome/src/dataType.js
@@ -10,25 +10,37 @@ var arr4 = [3, 3];
 // console.log(arr4);
 // 元祖
 var tuple = [12, "Nicholas Zakas"];
-var t = ["Nicholas", 18, [4, 5]];
 var t2 = ["Hanmeimei", 16, ["apple", "banana"]];
 var t3 = ["LiLei", 12, [3, 4]];
-// t.forEach((item) => {
-//     console.log(item);
-// });
+var t = ["Nicholas", 18, [4, 5]];
 t.push(99);
 console.log(t);
 console.log(t[2]);
-console.log(t[3]);
+// console.log(t[3]);
 // 函数
-var add = function (x, y) {
-    return x + y;
+var add = function (x, y) { return x + y; };
+var increment = function (x, y) { return x + y; };
+function decrement(x, y) {
+    return x - y;
+}
+// 声明了一个函数类型的变量，返回值为string，没有做实现
+var compute;
+// 做变量的函数的实现,做函数类型的实现的时候，形参可以不加类型注解，如a、b都没有加类型注解
+compute = function (a, b) {
+    return (a + b).toString();
 };
+console.log(compute(2, 3)); // 5
 // 对象
+// let obj:object = {
+//     x:1,
+//     y:2
+// };
+// obj.x;
 var obj = {
-    x: 1,
-    y: 2
+    x: 12,
+    y: 10
 };
+console.log(obj.x);
 // symbol
 var s1 = Symbol();
 var s2 = Symbol();
--- welcome/src/dataType.ts
+++ welcome/src/dataType.ts
@@ -21,21 +21,40 @@ let t:[string,number,Array<number>] = ["Nicholas",18,[4,5]];
 t.push(99);
 console.log(t);
 console.log(t[2]);
-console.log(t[3]);
+// console.log(t[3]);
 
 
 
 
 // 函数
-let add = (x:number,y:number):number => {
-    return x + y;
+let add = (x:number,y:number):number => x + y;
+
+let increment = (x:number,y:number) => x + y;
+
+function decrement(x:number,y:number):number{
+    return x - y;
 }
 
+// 声明了一个函数类型的变量，返回值为string，没有做实现
+let compute:(x:number,y:number) => string;
+// 做变量的函数的实现,做函数类型的实现的时候，形参可以不加类型注解，如a、b都没有加类型注解
+compute = (a,b) => {
+    return (a + b).toString();
+}
+console.log(compute(2,3)); // 5
+
 // 对象
-let obj:object = {
-    x:1,
-    y:2
+// let obj:object = {
+//     x:1,
+//     y:2
+// };
+// obj.x;
+let obj:{x:number,y:number} = {
+    x: 12,
+    y: 10
 };
+console.log(obj.x); // 12
+
 
 // symbol
 let s1:symbol = Symbol();
