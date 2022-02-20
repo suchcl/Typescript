d9d6eb00060f352fba2a40c403b2bf6d0a1c4e67
DESKTOP-MHSJV2V\ya596 <ya5969@163.com>
2022-02-20 20:48:15 +0800

    可索引类型接口

--- welcome/src/idata.ts
+++ welcome/src/idata.ts
@@ -2,7 +2,8 @@ interface List {
     // 定义一个只读属性id
     readonly id:number;
     name:string;
-    // [x:string]:any
+    [x:string]:any, // 字符串类型索引签名
+    email?:string // 可选属性
 }
 
 interface Result{
@@ -12,9 +13,16 @@ interface Result{
 function render(result:Result){
     result.data.forEach((item) => {
         console.log(`id: ${item.id}, name: ${item.name}`);
+        // item.id++; // 修改只读属性的值，报错了
         // if(item.gender){
         //     console.log(item.gender);
         // }
+        if(item.age){
+            console.log(`age: ${item.age}`);
+        }
+        if(item.email){
+            console.log(item.email);
+        }
     });
 }
 
@@ -24,16 +32,18 @@ const result = {
         {
             id:1,
             name: "Nicholas Zakas",
-            gender: "male"
+            gender: "male",
+            age: 16
         },
         {
             id: 2,
-            name: "Hameimei"
+            name: "Hameimei",
+            email: "xxx@xxx.com"
         }
     ]
 };
 
-// render(result);
+render(result);
 
 
 // render({
@@ -51,16 +61,39 @@ const result = {
 // } as Result);
 
 
-render(<Result>{
-    data: [
-        {
-            id:1,
-            name: "Nicholas Zakas",
-            gender: "female"
-        },
-        {
-            id: 2,
-            name: "Hameimei"
-        }
-    ]
-})
+// render(<Result>{
+//     data: [
+//         {
+//             id:1,
+//             name: "Nicholas Zakas",
+//             gender: "female"
+//         },
+//         {
+//             id: 2,
+//             name: "Hameimei"
+//         }
+//     ]
+// })
+
+
+interface StringArray{
+    // 数字索引签名
+    [index:number]:string //相当于声明了一个字符串类型数组的类型定义
+}
+let stringArray: StringArray;
+stringArray = ["a","b"];
+
+interface CustomObject{
+    // 字符串索引签名
+    [x:string]:string,
+    [index:number]: string
+}
+let obj2:CustomObject = {
+    username: "Nicholas Zakas",
+    age: "18",
+    mobile: "13265984568",
+    2:"数字"
+}
+console.log(obj2.age);
+console.log(obj2[2]);
+
