3d0f574cbd8016ca1ce1134a49a4aed01788b202
DESKTOP-MHSJV2V\ya596 <ya5969@163.com>
2022-02-21 22:25:48 +0800

    接口声明函数类型

--- welcome/src/idata.ts
+++ welcome/src/idata.ts
@@ -97,3 +97,55 @@ let obj2:CustomObject = {
 console.log(obj2.age);
 console.log(obj2[2]);
 
+
+// 这一行没有搞懂，怎么使用这个类型呢？
+let sum:(x:number,y:number) => number;
+
+
+// 用接口定义一个函数类型,
+interface Adds{
+    (x:number,y:number):number
+}
+let ads:Adds = (a,b) => a + b;
+
+// 使用类型别名定义函数
+type Add = (x:number,y:number) => number;
+let ad:Add = (a,b) => a - b;
+console.log("add：",ad(5,2)); // 3
+
+// 定义混合类型接口
+interface Lib {
+    // void返回值
+    ():void;
+    // string类型版本号
+    version: string;
+    // 定义一个方法
+    doSth():void;
+}
+// 实现这个混合类型接口
+let lib:Lib = (() => {}) as Lib;
+lib.version = "1.0.0";
+lib.doSth = () => {};
+
+// 可以尝试封装这个单例模式的获取库版本号的方法
+function getLib(){
+    let lib:Lib = (() => {}) as Lib;
+    lib.version = '1.2.0';
+    lib.doSth = () => {
+        console.log('来干活了');
+    };
+    return lib;
+}
+
+let l1 = getLib(); // 实例化一个对象
+console.log(l1.version); // 1.2.0
+l1.doSth(); // 来干活了
+
+// 可选参数y
+function add2(x:number,y?:number){
+    // 如果y存在，则返回x + y，否则返回x
+    return y? x + y : x;
+}
+
+console.log(add2(3,4)); // 7
+console.log(add2(9)); // 9
