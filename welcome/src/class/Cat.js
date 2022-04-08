d9b66529cdc564bfc79dff7799ba0fb946ab2097
suchcl <ya5969@163.com>
2022-04-08 23:27:44 +0800

    类的成员修饰符

--- welcome/src/class/Cat.js
+++ welcome/src/class/Cat.js
@@ -1,11 +1,67 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Cat = /** @class */ (function () {
     function Cat(name, age) {
         this.name = name;
         this.age = age;
     }
     Cat.prototype.run = function () { };
+    Cat.prototype.play = function () {
+        console.log("playing");
+    };
+    Cat.prototype.eat = function () {
+        console.log("hava supper!");
+    };
     return Cat;
 }());
-console.log(Cat.prototype);
 var cat = new Cat("Miaoxingren", 12);
-console.log(cat);
+// cat.play();
+console.log(Cat.prototype); // {run: ƒ, constructor: ƒ} Cat类的原型上只有方法，定义的构造函数和成员函数
+console.log(cat); // Cat {name: 'Miaoxingren', age: 12} 只有Cat类的成员属性name和age
+var MiaoMiao = /** @class */ (function (_super) {
+    __extends(MiaoMiao, _super);
+    function MiaoMiao(name, age, color) {
+        var _this = _super.call(this, name, age) || this;
+        // 派生类中成员属性初始化的时候，一定要在super之后，如this.color在super之后
+        _this.color = color;
+        _this.eat();
+        return _this;
+    }
+    return MiaoMiao;
+}(Cat));
+var miao = new MiaoMiao("Miaomiao", 12, "yellow");
+console.log(miao);
+var People = /** @class */ (function () {
+    function People(name, age) {
+        this.name = name;
+        this.age = age;
+        this.name = name;
+        this.age = age;
+    }
+    People.color = "yello";
+    return People;
+}());
+var pl = new People("Nicholas Zakas", 16);
+console.log(People.color); // yello
+console.log(pl); // People {name: 'Nicholas Zakas', age: 16}
+var WhitePeople = /** @class */ (function (_super) {
+    __extends(WhitePeople, _super);
+    function WhitePeople(name, age) {
+        return _super.call(this, name, age) || this;
+    }
+    return WhitePeople;
+}(People));
+console.log(WhitePeople.color);
--- welcome/src/class/Cat.ts
+++ welcome/src/class/Cat.ts
@@ -9,11 +9,14 @@ class Cat {
     private play() {
         console.log("playing");
     }
+    protected eat() {
+        console.log("hava supper!");
+
+    }
 }
 
-
 const cat = new Cat("Miaoxingren", 12);
-cat.play();
+// cat.play();
 
 console.log(Cat.prototype); // {run: ƒ, constructor: ƒ} Cat类的原型上只有方法，定义的构造函数和成员函数
 console.log(cat); // Cat {name: 'Miaoxingren', age: 12} 只有Cat类的成员属性name和age
@@ -24,7 +27,32 @@ class MiaoMiao extends Cat {
         super(name, age);
         // 派生类中成员属性初始化的时候，一定要在super之后，如this.color在super之后
         this.color = color;
+        this.eat();
     }
     // 为派生类添加自己的属性color，在
     color: string;
 }
+
+const miao = new MiaoMiao("Miaomiao", 12, "yellow");
+console.log(miao);
+
+
+class People {
+    constructor(public name: string, public age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    static color: string = "yello";
+}
+
+const pl = new People("Nicholas Zakas", 16);
+console.log(People.color); // yello
+
+console.log(pl); // People {name: 'Nicholas Zakas', age: 16}
+
+class WhitePeople extends People {
+    constructor(name: string, age: number) {
+        super(name, age);
+    }
+}
+console.log(WhitePeople.color); // 派生类访问了父类中定义的static成员变量
